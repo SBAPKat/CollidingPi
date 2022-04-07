d6d20ed65553b786d9c9112fc68476e9f08d9a54
SBAPKat <sbapkat@LAPTOP-5H3CEUN7.localdomain>
2022-04-07 18:19:14 +0200

    implementation of pow() + progress prints

--- pi.c
+++ pi.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
 
+unsigned long long power(int a, int b);
 
 int main(int argc, char *argv[])
 {
@@ -13,16 +13,19 @@ int main(int argc, char *argv[])
 		fprintf(stderr,"Please provide only one argument\n");
 		return -1;
 	}
-	unsigned long long  main_mass=10;
+	unsigned long long  main_mass=10, check_value;
 	int ratio, secondary_mass=1;
 	double main_vel=1, secondary_vel=0, vcm;
 	unsigned long  collisions=0;
-	ratio=atoi(argv[1]);
+	char *ptr;
+	ratio=strtod(argv[1], &ptr);
 	if(ratio<=0){
 		fprintf(stderr,"Please provide a correct, positive number\n");
 		return -1;
 	}
-	main_mass=pow(100,ratio);
+	main_mass=power(100,ratio);
+	printf("secondary mass is %i, main mass is %llu, selected ratio is %i\n", secondary_mass, main_mass, ratio);
+	check_value = power(10, ratio - 1); 
 	do
 	{
 		//cubes collide
@@ -31,10 +34,11 @@ int main(int argc, char *argv[])
 		secondary_vel = 2*vcm-secondary_vel;
 		collisions++;
 		if(secondary_vel>0){
-		//wall and cube collide
-		secondary_vel = secondary_vel*(-1);
-		collisions ++;}
-
+			//wall and cube collide
+			secondary_vel = secondary_vel*(-1);
+			collisions ++;
+			if(collisions % check_value == 0 ) printf("there have been %lu collisions so far\n", collisions);
+		}
 	}
 	while( secondary_vel < main_vel ||( secondary_vel>0 && main_vel > 0));
 	printf("There are %lu collisions\n",collisions);
@@ -42,4 +46,15 @@ int main(int argc, char *argv[])
 	return 0;
 
 
+}
+
+unsigned long long power(int a, int b)
+{
+	unsigned long long result;
+	result = a;
+	for(int i = 0; i < b; i++)
+	{
+		result = result * a;
+	} 
+	return result;
 }
